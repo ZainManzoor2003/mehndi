852a6085ca151b41b30f464752338cfea04d4882
ZainManzoor2003 <zainmanzoor2003@gmail.com>
2025-09-29 15:25:50 +0500

    Update API base URL to point to the production server and adjust CORS configuration to allow requests from the new localhost URL.

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser');
 const db = require('./config/db')
 
 const corsOptions = {
-  origin: ['http://localhost:3000','https://mehndi-client.vercel.app'],
+  origin: ['http://localhost:3000','https://mehndi-client.vercel.app','http://localhost:5001'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
